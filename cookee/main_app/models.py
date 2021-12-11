af19d4b2e17584d7fe2b8012c6c808fe56465d13
mikolaj200293 <mikolaj200293@gmail.com>
2021-12-11 11:36:53 +0100

    Add models documentation

--- cookee/main_app/models.py
+++ cookee/main_app/models.py
@@ -12,6 +12,9 @@ MEALS = (
 
 
 class ProductCategory(models.Model):
+    """
+    Product Category model.
+    """
     category_name = models.CharField(max_length=100, unique=True, verbose_name='Kategoria')
 
     def __str__(self):
@@ -19,6 +22,9 @@ class ProductCategory(models.Model):
 
 
 class Product(models.Model):
+    """
+    Product model.
+    """
     product_name = models.CharField(max_length=100, verbose_name='Nazwa produktu')
     proteins = models.DecimalField(null=True, verbose_name='Białka', decimal_places=2, max_digits=6)
     carbohydrates = models.DecimalField(null=True, verbose_name='Węglowodany', decimal_places=2, max_digits=6)
@@ -28,6 +34,9 @@ class Product(models.Model):
 
     @property
     def calories(self):
+        """
+        Calculate product calories.
+        """
         calories_calculated = self.proteins * 4 + self.carbohydrates * 4 + self.fats * 9
         return round(calories_calculated, 2)
 
@@ -36,6 +45,9 @@ class Product(models.Model):
 
 
 class Recipe(models.Model):
+    """
+    Recipe model.
+    """
     recipe_name = models.CharField(max_length=100, unique=True, verbose_name='Nazwa przepisu')
     description = models.TextField(verbose_name='Opis przygotowania')
     preparation_time = models.IntegerField(verbose_name='Czas przygotowania')
@@ -46,6 +58,9 @@ class Recipe(models.Model):
 
     @property
     def recipe_calories(self):
+        """
+        Calculate recipe calories.
+        """
         calories_calculated = 0
         for product in self.products.all():
             calories_calculated += product.calories * product.productsquantities_set.\
@@ -54,14 +69,19 @@ class Recipe(models.Model):
 
     @property
     def portion_calories(self):
+        """
+        Calculate 1 portion of recipe meal calories.
+        """
         return round(self.recipe_calories / self.portions, 2)
 
     def __str__(self):
-        return f"{self.recipe_name}" \
-               # f"(4 porcje: {self.recipe_calories} kcal)"
+        return f"{self.recipe_name}"
 
 
 class Persons(models.Model):
+    """
+    Persons model.
+    """
     name = models.CharField(max_length=100, verbose_name='Imię')
     calories = models.IntegerField(verbose_name='Zapotrzebowanie na kalorie')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -71,6 +91,9 @@ class Persons(models.Model):
 
 
 class Plan(models.Model):
+    """
+    Plan model.
+    """
     plan_name = models.CharField(max_length=100, verbose_name='Nazwa planu')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     plan_length = models.IntegerField(verbose_name='Długość planu')
@@ -79,6 +102,9 @@ class Plan(models.Model):
 
     @property
     def plan_calories(self):
+        """
+        Calculate plan calories to cover by meals.
+        """
         result = 0
         for person in self.persons.all():
             result += person.calories
@@ -89,6 +115,9 @@ class Plan(models.Model):
 
 
 class Meal(models.Model):
+    """
+    Meal model.
+    """
     plan_name = models.ManyToManyField(Plan, verbose_name='Nazwa planu')
     plan_day = models.IntegerField(verbose_name='Dzień planu')
     meal = models.IntegerField(choices=MEALS, null=True, verbose_name='Posiłek')
@@ -99,6 +128,9 @@ class Meal(models.Model):
 
     @property
     def meal_calories(self):
+        """
+        Calculate meal calories.
+        """
         result = self.recipes.portion_calories * self.meal_portions
         return result
 
@@ -107,17 +139,26 @@ class Meal(models.Model):
 
 
 class ProductsQuantities(models.Model):
+    """
+    Products quantities model.
+    """
     product_quantity = models.DecimalField(default=0, verbose_name='Ilość produktu', decimal_places=1, max_digits=8)
     recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
 
     @property
     def one_portion_product_quantity(self):
+        """
+        Calculate quantity of product in one portion of related recipe.
+        """
         result = self.product_quantity / self.recipe_id.portions
         return result
 
 
 class ShoppingList(models.Model):
+    """
+    Shopping list model.
+    """
     plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, through='ShoppingListProducts')
     date_created = models.DateTimeField(auto_created=True, auto_now=True)
@@ -125,6 +166,9 @@ class ShoppingList(models.Model):
 
 
 class ShoppingListProducts(models.Model):
+    """
+    Shopping list products model.
+    """
     product_quantity = models.DecimalField(default=0, decimal_places=1, max_digits=8)
     shopping_list = models.ForeignKey(ShoppingList, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
